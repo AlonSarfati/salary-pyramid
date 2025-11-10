584d7336b2a57c11330f3f84f2cf3fcf98a899f6
alon sarfati <alonsarfati3@gmail.com>
2025-11-10 11:48:03 +0200

    starting ui

---
+++ ui/src/App.js
@@ -0,0 +1,8 @@
+import React from "react";
+import AtlasSolutionsLanding from "./AtlasSolutionsLanding"; // not 'landind' and case-sensitive
+
+function App() {
+  return <AtlasSolutionsLanding />;
+}
+
+export default App;
